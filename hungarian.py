72fa60ff23373486228e7ca5641e989c099e119f
Dicky <87747688+diky091720@users.noreply.github.com>
2024-01-04 22:35:18 +0700

    Update hungarian.py

--- hungarian.py
+++ hungarian.py
@@ -6,25 +6,26 @@ from sklearn.metrics import accuracy_score
 import streamlit as st
 import time
 import pickle
-
+#Load Data Hungarian.data
 with open("hungarian.data", encoding='Latin1') as file:
   lines = [line.strip() for line in file]
-
+#Melihat data frame
 data = itertools.takewhile(
   lambda x: len(x) == 76,
   (' '.join(lines[i:(i + 10)]).split() for i in range(0, len(lines), 10))
 )
 
 df = pd.DataFrame.from_records(data)
-
+#Penghapusan nilai null
 df = df.iloc[:, :-1]
 df = df.drop(df.columns[0], axis=1)
+#Merubah Nilai menjadi float
 df = df.astype(float)
-
+#Menganti nilai -9.0
 df.replace(-9.0, np.NaN, inplace=True)
-
+#Memilih attribute yang digunakan
 df_selected = df.iloc[:, [1, 2, 7, 8, 10, 14, 17, 30, 36, 38, 39, 42, 49, 56]]
-
+#Memberikan nama tiap kolom
 column_mapping = {
   2: 'age',
   3: 'sex',
@@ -43,31 +44,31 @@ column_mapping = {
 }
 
 df_selected.rename(columns=column_mapping, inplace=True)
-
+#Mengapus kolom ca,slope,dan thal
 columns_to_drop = ['ca', 'slope','thal']
 df_selected = df_selected.drop(columns_to_drop, axis=1)
-
+#Menghitung nilai rata"
 meanTBPS = df_selected['trestbps'].dropna()
 meanChol = df_selected['chol'].dropna()
 meanfbs = df_selected['fbs'].dropna()
 meanRestCG = df_selected['restecg'].dropna()
 meanthalach = df_selected['thalach'].dropna()
 meanexang = df_selected['exang'].dropna()
-
+#Merubah tipe data menjadi float
 meanTBPS = meanTBPS.astype(float)
 meanChol = meanChol.astype(float)
 meanfbs = meanfbs.astype(float)
 meanthalach = meanthalach.astype(float)
 meanexang = meanexang.astype(float)
 meanRestCG = meanRestCG.astype(float)
-
+#Membulatkan nilai rata"
 meanTBPS = round(meanTBPS.mean())
 meanChol = round(meanChol.mean())
 meanfbs = round(meanfbs.mean())
 meanthalach = round(meanthalach.mean())
 meanexang = round(meanexang.mean())
 meanRestCG = round(meanRestCG.mean())
-
+#Mengisi nilai value dengan rata" yang sebelumnya telah dihitung
 fill_values = {
   'trestbps': meanTBPS,
   'chol': meanChol,
@@ -78,16 +79,17 @@ fill_values = {
 }
 
 df_clean = df_selected.fillna(value=fill_values)
+#Membersihkan data yang terduplikasi
 df_clean.drop_duplicates(inplace=True)
-
+#Menentukan variabel input dan target
 X = df_clean.drop("target", axis=1)
 y = df_clean['target']
-
+#Membuat oversampling dengan smote
 smote = SMOTE(random_state=42)
 X, y = smote.fit_resample(X, y)
-
+#Memanggil model xgboost untuk hasil prediksi
 model = pickle.load(open("xgb_model.pkl", 'rb'))
-
+#Menentukan nilai akurasi
 y_pred = model.predict(X)
 accuracy = accuracy_score(y, y_pred)
 accuracy = round((accuracy * 100), 2)
@@ -98,24 +100,26 @@ df_final['target'] = y
 # ========================================================================================================================================================================================
 
 # STREAMLIT
+#Menganti title dan icon pada web streamlit
 st.set_page_config(
   page_title = "Hungarian-Heart-Disease",
   page_icon = ":skull:"
 )
-
+#Memberikan tittle
 st.title(":heart: :red[Hungarian Heart Disease] :heart:")
 st.write(f"Model Akurasi :  :green[**{accuracy}**]%")
 st.write("")
-
+#Membuat 2 tab
 tab1, tab2 = st.tabs(["[Single]", "[Multi]"])
 
 with tab1:
+  #Membuat sidebar untuk menginput data
   st.sidebar.header("Input Data")
-
+  #Memasukan input nilai umur
   age = st.sidebar.number_input(label=":blue[**Age**]", min_value=df_final['age'].min(), max_value=df_final['age'].max())
   st.sidebar.write(f":orange[Min] value: :orange[**{df_final['age'].min()}**], :red[Max] value: :red[**{df_final['age'].max()}**]")
   st.sidebar.write("")
-
+  #Memasukan jenis kelamin
   sex_sb = st.sidebar.selectbox(label=":blue[**Sex**]", options=["Male", "Female"])
   st.sidebar.write("")
   st.sidebar.write("")
@@ -125,7 +129,7 @@ with tab1:
     sex = 0
   # -- Value 0: Female
   # -- Value 1: Male
-
+  #Memilih jenis nyeri pada dada
   cp_sb = st.sidebar.selectbox(label=":blue[**Chest pain type**]", options=["Typical angina", "Atypical angina", "Non-anginal pain", "Asymptomatic"])
   st.sidebar.write("")
   st.sidebar.write("")
@@ -141,15 +145,15 @@ with tab1:
   # -- Value 2: atypical angina
   # -- Value 3: non-anginal pain
   # -- Value 4: asymptomatic
-
+  #Input nilai tensi darah
   trestbps = st.sidebar.number_input(label=":blue[**Resting blood pressure** (in mm Hg on admission to the hospital)]", min_value=df_final['trestbps'].min(), max_value=df_final['trestbps'].max())
   st.sidebar.write(f":orange[Min] value: :orange[**{df_final['trestbps'].min()}**], :red[Max] value: :red[**{df_final['trestbps'].max()}**]")
   st.sidebar.write("")
-
+  #Input Nilai Kolesterol
   chol = st.sidebar.number_input(label=":blue[**Serum cholestoral** (in mg/dl)]", min_value=df_final['chol'].min(), max_value=df_final['chol'].max())
   st.sidebar.write(f":orange[Min] value: :orange[**{df_final['chol'].min()}**], :red[Max] value: :red[**{df_final['chol'].max()}**]")
   st.sidebar.write("")
-
+  #Input nilai tes gula
   fbs_sb = st.sidebar.selectbox(label=":blue[**Fasting blood sugar > 120 mg/dl?**]", options=["False", "True"])
   st.sidebar.write("")
   st.sidebar.write("")
@@ -159,7 +163,7 @@ with tab1:
     fbs = 1
   # -- Value 0: false
   # -- Value 1: true
-
+  #Input apakah ada kelainan pada jantung
   restecg_sb = st.sidebar.selectbox(label=":blue[**Resting electrocardiographic results**]", options=["Normal", "Having ST-T wave abnormality", "Showing left ventricular hypertrophy"])
   st.sidebar.write("")
   st.sidebar.write("")
@@ -172,11 +176,11 @@ with tab1:
   # -- Value 0: normal
   # -- Value 1: having ST-T wave abnormality (T wave inversions and/or ST  elevation or depression of > 0.05 mV)
   # -- Value 2: showing probable or definite left ventricular hypertrophy by Estes' criteria
-
+  #Input nilai detang jantung tertinggi
   thalach = st.sidebar.number_input(label=":blue[**Maximum heart rate achieved**]", min_value=df_final['thalach'].min(), max_value=df_final['thalach'].max())
   st.sidebar.write(f":orange[Min] value: :orange[**{df_final['thalach'].min()}**], :red[Max] value: :red[**{df_final['thalach'].max()}**]")
   st.sidebar.write("")
-
+  #Input apakah ada penyakit induced angina?
   exang_sb = st.sidebar.selectbox(label=":blue[**Exercise induced angina?**]", options=["No", "Yes"])
   st.sidebar.write("")
   st.sidebar.write("")
@@ -186,11 +190,11 @@ with tab1:
     exang = 1
   # -- Value 0: No
   # -- Value 1: Yes
-
+  #Input nilai depresi ST yang diakibatkan oleh latihan relative terhadap saat istirahat
   oldpeak = st.sidebar.number_input(label=":blue[**ST depression induced by exercise relative to rest**]", min_value=df_final['oldpeak'].min(), max_value=df_final['oldpeak'].max())
   st.sidebar.write(f":orange[Min] value: :orange[**{df_final['oldpeak'].min()}**], :red[Max] value: :red[**{df_final['oldpeak'].max()}**]")
   st.sidebar.write("")
-
+  #Menyimpan nilai ke dataframe dari hasil input
   data = {
     'Age': age,
     'Sex': sex_sb,
@@ -203,9 +207,9 @@ with tab1:
     'Exercise induced angina?': exang_sb,
     'ST depression': oldpeak,
   }
-
+  #Menampilkan hasil input sebelumnya ke dataframe
   preview_df = pd.DataFrame(data, index=['input'])
-
+  
   st.header("Detail Input Pengguna")
   st.write("")
   st.dataframe(preview_df.iloc[:, :6])
