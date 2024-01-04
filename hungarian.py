6a1e669270752b1295154f511e7f8f3a2a28f963
Dicky <87747688+diky091720@users.noreply.github.com>
2024-01-04 20:08:30 +0700

    Update hungarian.py

--- hungarian.py
+++ hungarian.py
@@ -104,7 +104,7 @@ st.set_page_config(
 )
 
 st.title("Hungarian Heart Disease :heart:")
-st.write(f"**Akurasi Model** :  :green[**{accuracy}**]%
+st.write(f"**Akurasi Model** :  :green[**{accuracy}**]%)
 st.write("")
 
 tab1, tab2 = st.tabs(["Single-predict", "Multi-predict"])
