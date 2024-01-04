90694138db5fe3a2f644693ac703883f32c3d942
Dicky <87747688+diky091720@users.noreply.github.com>
2024-01-04 21:57:37 +0700

    Update hungarian.py

--- hungarian.py
+++ hungarian.py
@@ -112,7 +112,7 @@ tab1, tab2 = st.tabs(["[Single]", "[Multi]"])
 with tab1:
   st.sidebar.header("Input Data")
 
-  age = st.sidebar.number_input(label=":rainbow[**Age**]", min_value=df_final['age'].min(), max_value=df_final['age'].max())
+  age = st.sidebar.number_input(label=":blue[**Age**]", min_value=df_final['age'].min(), max_value=df_final['age'].max())
   st.sidebar.write(f":orange[Min] value: :orange[**{df_final['age'].min()}**], :red[Max] value: :red[**{df_final['age'].max()}**]")
   st.sidebar.write("")
 
