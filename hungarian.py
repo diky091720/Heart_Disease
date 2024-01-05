a3308a0573e77beaf0e5b827767da22c423a9a0c
Dicky <87747688+diky091720@users.noreply.github.com>
2024-01-05 09:25:59 +0700

    Update hungarian.py

--- hungarian.py
+++ hungarian.py
@@ -108,9 +108,9 @@ st.set_page_config(
 #Memberikan tittle
 st.title(":heart: :red[Hungarian Heart Disease] :heart:")
 if accuracy > 80:
-  st.write(f"Model Akurasi :  :green[**{accuracy}**]% :green[High]:like:")
+  st.write(f"Model Akurasi :  :green[**{accuracy}**]% :green[High]:up:")
 elif accuracy < 79:
-  st.write(f"Model Akurasi :  :red[**{accuracy}**]% :red[Low]")
+  st.write(f"Model Akurasi :  :red[**{accuracy}**]% :red[Low]:down:")
 st.write("Jenis Model : :blue[**XGBOOST**]")
 st.write("")
 #Membuat 2 tab
